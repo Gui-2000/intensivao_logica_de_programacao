cb160b9213ddda79ef170eb083160eba85149463
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-06-09 15:12:02 -0300

    Created activity 8

---
+++ src/listaDeAtividade1/Atividade8.java
@@ -0,0 +1,15 @@
+package listaDeAtividade1;
+
+public class Atividade8 {
+    // 8. Faca um algoritmo para transformar uma distância expressa em milhas para quilômetros.
+    //Sabe-se que uma milha corresponde a 0,6214 km.
+
+    public static void main(String[] args) {
+
+        int milhas = 10 ;
+
+        double quilômetros = milhas * 1.609344;
+
+        System.out.println(quilômetros);
+    }
+}
