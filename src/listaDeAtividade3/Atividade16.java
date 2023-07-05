fda560935ac1d3952913ef9d926cfcd0a3f0eeee
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-07-05 14:31:15 -0300

    Created activity 16 of list3

---
+++ src/listaDeAtividade3/Atividade16.java
@@ -0,0 +1,18 @@
+package listaDeAtividade3;
+
+public class Atividade16 {
+    //16. Construa um algoritmo que mostre na tela:
+    //*
+    //**
+    //***
+    //****
+    //*****
+    public static void main(String[] args) {
+
+        String letra = "*";
+        for (int i=0; i<5; i++) {
+            System.out.println(letra);
+            letra = letra + "*";
+        }
+    }
+}
