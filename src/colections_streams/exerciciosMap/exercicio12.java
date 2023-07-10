f60cb03ae2a9895bc85ffe326c2ee2edfbad9cfe
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-07-10 16:28:26 -0300

    Created exercicio12 of colections/exerciciosMap

---
+++ src/colections_streams/exerciciosMap/exercicio12.java
@@ -0,0 +1,24 @@
+package colections_streams.exerciciosMap;
+
+import java.util.TreeMap;
+
+public class exercicio12 {
+    //usando o treeMap;
+    //12:Retorna o antecessor da arvore passada como referencia no parametro metodo lowerKey(referencia);
+    public static void main(String[] args) {
+
+        TreeMap<String, String> protagonistasDasSeries = new TreeMap<>();
+
+        protagonistasDasSeries.put("Suits", "Mike Ross");
+        protagonistasDasSeries.put("Game Of Thores", "Jon Snow");
+        protagonistasDasSeries.put("Peaky Blinders", "Tommy Shelby");
+        protagonistasDasSeries.put("The Boys", "Bruto");
+        protagonistasDasSeries.put("Stranger Things", "Eleven");
+        protagonistasDasSeries.put("The Walking Dead", "Rick Grimes");
+        protagonistasDasSeries.put("Bridgerton", "Daphne Bridgerton");
+        protagonistasDasSeries.put("Vikings", "Ragnar");
+
+        System.out.println("A Serie que antecede Stranger Things é: " + protagonistasDasSeries.lowerKey("Stranger Things"));
+    }
+}
+
