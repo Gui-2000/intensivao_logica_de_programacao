91f6b73c5dc230db813e545f0d29651364f8c7b6
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-07-10 16:20:24 -0300

    Created exercicio10 of colections/exerciciosMap

---
+++ src/colections_streams/exerciciosMap/exercicio10.java
@@ -0,0 +1,26 @@
+package colections_streams.exerciciosMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class exercicio10 {
+    //usando o treeMap;
+    //10:Retorna o topo da arvore metodo firstKey();
+    public static void main(String[] args) {
+
+        TreeMap<String, String> protagonistasDasSeries = new TreeMap<>();
+
+        protagonistasDasSeries.put("Suits", "Mike Ross");
+        protagonistasDasSeries.put("Game Of Thores", "Jon Snow");
+        protagonistasDasSeries.put("Peaky Blinders", "Tommy Shelby");
+        protagonistasDasSeries.put("The Boys", "Bruto");
+        protagonistasDasSeries.put("Stranger Things", "Eleven");
+        protagonistasDasSeries.put("The Walking Dead", "Rick Grimes");
+        protagonistasDasSeries.put("Bridgerton", "Daphne Bridgerton");
+        protagonistasDasSeries.put("Vikings", "Ragnar");
+
+        System.out.println("Primeira Serie é: " + protagonistasDasSeries.firstKey());
+    }
+}
+
